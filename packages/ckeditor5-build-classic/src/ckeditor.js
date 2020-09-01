cfd5772a17d5f016ef3577442dfcb2f089fae173
Sowz <hadrien.diot@gmail.com>
2020-09-01 07:46:52 +0200

    add mentions to classic build

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
+
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +58,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Mention
 ];
 
 // Editor configuration.
@@ -98,5 +101,5 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'fr'
 };
